ea0a7fca9c07994196ff7802622c9978a2bde7e8
arfan38 <arfan0206@gmail.com>
2026-04-26 23:51:25 +0800

    Add query to aggregate top paying skills for Data Analyst roles

--- project_sql/4_top_paying_skills.sql
+++ project_sql/4_top_paying_skills.sql
@@ -5,3 +5,16 @@ Question: What are the top skills base on salary?
 - Why? It reveals how different skills impact salary levels for Data Analyst and
     helps identify the most financially rewarding skills to aquire or improve
 */
+
+SELECT
+    ROUND (AVG (salary_year_avg),2) AS avg_salary,
+    skills
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON skills_job_dim.job_id = job_postings_fact.job_id
+INNER JOIN skills_dim ON skills_dim.skill_id = skills_job_dim.skill_id
+WHERE 
+    job_title_short = 'Data Analyst' AND
+    salary_year_avg IS NOT NULL
+GROUP BY skills
+ORDER BY avg_salary DESC
+LIMIT 25;
