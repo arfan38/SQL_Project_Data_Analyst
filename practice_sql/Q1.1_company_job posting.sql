84c712016f229377016c756845cc42c7524267f1
arfan38 <arfan0206@gmail.com>
2026-05-05 23:35:55 +0800

    update practice

--- practice_sql/Q1.1_company_job posting.sql
+++ practice_sql/Q1.1_company_job posting.sql
@@ -6,3 +6,13 @@ Question: Which companies have the highest number of job postings for Data Analy
 - Join with company_dim to get company names
 - Why? Identifies companies actively hiring Data Analysts most frequently
 */
+
+SELECT
+    company_dim.name AS company_name,
+    COUNT (job_postings_fact.job_id) AS total_job
+FROM job_postings_fact
+INNER JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_title_short = 'Data Analyst'
+GROUP BY name
+ORDER BY total_job DESC;
