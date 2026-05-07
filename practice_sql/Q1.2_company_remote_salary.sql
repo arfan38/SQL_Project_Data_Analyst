6bacf68a69407ac365d999663abf92d2e39cb395
arfan38 <arfan0206@gmail.com>
2026-05-07 23:01:54 +0800

    update practice

--- practice_sql/Q1.2_company_remote_salary.sql
+++ practice_sql/Q1.2_company_remote_salary.sql
@@ -1,6 +1,24 @@
 /*
-Question: Which companies post remote jobs with the highest salaries?
+Question: Which companies post remote jobs for 'Data Analyst' with the highest salaries?
+- Average the salary_year_avg based on company
 - Filter job_work_from_home = TRUE
 - Group by company_id and salary
 - Why? Identifies top remote-friendly employers in the data market
 */
+
+SELECT
+    name,
+    ROUND (AVG (salary_year_avg),2) AS company_avg_salary,
+    COUNT (job_id) AS job_count
+FROM
+    job_postings_fact
+INNER JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_work_from_home = TRUE AND
+    job_title_short = 'Data Analyst' AND
+    salary_year_avg IS NOT NULL
+GROUP BY
+    name
+ORDER BY company_avg_salary DESC
+LIMIT 25;
+
--- practice_sql/Q1.3_company_salary range.sql
+++ practice_sql/Q1.3_company_salary range.sql
@@ -3,3 +3,16 @@ Question: Which companies offer the widest salary range for Data Analyst roles?
 - Calculate MAX(salary) - MIN(salary) per company
 - Why? Identifies companies with high internal salary variation (junior vs senior roles)
 */
+
+SELECT
+    name,
+    MAX(salary_year_avg) - MIN(salary_year_avg) AS salary_range
+FROM job_postings_fact
+INNER JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_title_short = 'Data Analyst' AND
+    salary_year_avg IS NOT NULL
+GROUP BY
+    name
+ORDER BY salary_range DESC
+LIMIT 25;
