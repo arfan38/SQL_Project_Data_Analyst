483f6cafc28b24a96406529bf0b8fadbb12bb007
arfan38 <arfan0206@gmail.com>
2026-05-09 13:32:52 +0800

    update practice

--- practice_sql/Q1.4_company_benefits.sql
+++ practice_sql/Q1.4_company_benefits.sql
@@ -2,5 +2,21 @@
 Question: Which companies combine remote work + high salary + benefits most often?
 - Filter job_work_from_home = TRUE
 - Check salary + job_health_insurance
+- BONUS: if want to filter for Data Analyst just remove '--' and add AND
 - Why? Finds “best overall job packages”
 */
+
+SELECT
+    name,
+    ROUND (AVG (salary_year_avg),2) AS avg_salary,
+    COUNT (job_id) AS total_job
+FROM job_postings_fact
+INNER JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_work_from_home = TRUE AND
+    job_health_insurance = TRUE AND
+    salary_year_avg IS NOT NULL
+--    job_title_short = 'Data Analyst'
+GROUP BY name
+ORDER BY avg_salary DESC
+LIMIT 50;
